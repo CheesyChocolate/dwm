abebba63b733d42b92d06b8c180bc557e1c04ebb
CheesyChocolate <dev@behnamlal.xyz>
2025-10-29 17:46:02 +0000

    feat: add dmenuemoticons keybinding

--- config.h
+++ config.h
@@ -145,7 +145,7 @@ static const Key keys[] = {
 	STACKKEYS(MODKEY|ShiftMask,                push)
 	/* { MODKEY|ShiftMask,		XK_Escape,	spawn,	SHCMD("") }, */
 	{ MODKEY,			XK_grave,	spawn,	{.v = (const char*[]){ "dmenuunicode", NULL } } },
-	/* { MODKEY|ShiftMask,		XK_grave,	togglescratch,	SHCMD("") }, */
+	{ MODKEY|ShiftMask,		XK_grave,	spawn,	{.v = (const char*[]){ "dmenuemoticons", NULL } } },
 	TAGKEYS(			XK_1,		0)
 	TAGKEYS(			XK_2,		1)
 	TAGKEYS(			XK_3,		2)
